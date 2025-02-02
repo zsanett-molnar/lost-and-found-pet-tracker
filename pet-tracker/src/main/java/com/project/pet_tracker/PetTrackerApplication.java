4af11c7c3a628f8ac3083c261d62a741fd9cb77c
zsanett-molnar <96383347+zsanett-molnar@users.noreply.github.com>
2025-02-02 17:11:25 +0200

    init: project

---
+++ pet-tracker/src/main/java/com/project/pet_tracker/PetTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.project.pet_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTrackerApplication.class, args);
+	}
+
+}
---
+++ pet-tracker/src/test/java/com/project/pet_tracker/PetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pet_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
